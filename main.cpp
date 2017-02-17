ab19e03a73f0eb30d918182dda7997eb94fe4cef
besszerf <caruzzo74@gmail.com>
2017-02-17 10:06:40 +0100

    Comparing vectors is function template now

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <string>
 
 using namespace std;
 typedef unsigned int uint;
 
-bool compare (vector<float>& v1, vector<float>& v2, bool isOrderImportant = true) {
+template<class T>
+bool compare (vector<T>& v1, vector<T>& v2, bool isOrderImportant = true) {
   if(v1.size() != v2.size()) {
     return false;
   }
@@ -22,9 +24,13 @@ bool compare (vector<float>& v1, vector<float>& v2, bool isOrderImportant = true
 }
 
 int main(int argc, char* argv[]) {
-  vector<float> v1 = {2, 1, 3};
-  vector<float> v2 = {1, 2, 3};
+  vector<uint> v1 = {1, 2, 3};
+  vector<uint> v2 = {3, 2, 1};
+  vector<string> v3 = {"2", "1", "lo"};
+  vector<string> v4 = {"1", "2", "lo"};
   cout << (compare(v1, v2)?"Vector contents are the same.":"Vector contents are not the same.") << endl;
   cout << (compare(v1, v2, false)?"Vector contents are the same.":"Vector contents are not the same.") << endl;
+  cout << (compare(v3, v4)?"Vector contents are the same.":"Vector contents are not the same.") << endl;
+  cout << (compare(v3, v4, false)?"Vector contents are the same.":"Vector contents are not the same.") << endl;
   return 0;
 }
